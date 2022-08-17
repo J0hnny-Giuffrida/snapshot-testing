958780cf14086a0c786d04a1ae6c27a5bd1cfaa8
Johnny Giuffrida <jcgiuffridaii@gmail.com>
2022-08-16 22:45:59 -0400

    Finished Assignment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GitCard from './components/GitHubCard'
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
         >
           Learn React
         </a>
+        <h2>TESTING SNAPSHOT</h2>
+        <GitCard />
       </header>
     </div>
   );
---
+++ src/components/GitHubCard.js
@@ -0,0 +1,17 @@
+import Card from 'react-bootstrap/Card'
+
+function GitCard() {
+    return (
+        <Card>
+      <Card.Img variant="top" src="public/logo192.png" />
+      <Card.Body>
+        <Card.Title>J0hnny_Giuffrida</Card.Title>
+        <Card.Text>
+          Inspiring Software Engineer from North Carolina.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    )
+}
+
+export default GitCard
